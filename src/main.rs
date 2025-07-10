d64efe835084bcf5ca3e61ec6a42350ab1dc0939
ziv-y-chang <ziv_y_chang@trendmicro.com>
2025-07-10 12:12:53 +0800

    integrate poe into your runtime

--- src/main.rs
+++ src/main.rs
@@ -13,6 +13,7 @@ mod types {
     pub type Extrinsic = crate::support::Extrinsic<AccountId, crate::RuntimeCall>;
     pub type Header = crate::support::Header<BlockNumber>;
     pub type Block = crate::support::Block<Header, Extrinsic>;
+    pub type Content = &'static str;
 
 }
 
@@ -21,12 +22,14 @@ mod types {
 pub enum RuntimeCall {
 	// TODO: Not implemented yet.
     Balances(balances::Call<Runtime>),
+    ProofOfExistence(proof_of_existence::Call<Runtime>),
 }
 
 #[derive(Debug)]
 pub struct Runtime {
     system: system::Pallet<Self>,
     balances: balances::Pallet<Self>,
+    proof_of_existence: proof_of_existence::Pallet<Self>,
 }
 
 impl system::Config for Runtime {
@@ -39,9 +42,13 @@ impl balances::Config for Runtime {
 	type Balance = types::Balance;
 }
 
+impl proof_of_existence::Config for Runtime {
+	type Content = types::Content;
+}
+
 impl Runtime {
 	fn new() -> Self {
-        Self { system:  system::Pallet::new(), balances: balances::Pallet::new() }
+        Self { system:  system::Pallet::new(), balances: balances::Pallet::new(), proof_of_existence: proof_of_existence::Pallet::new(), }
 	}
 	// Execute a block of extrinsics. Increments the block number.
 	fn execute_block(&mut self, block: types::Block) -> support::DispatchResult {
@@ -81,6 +88,9 @@ impl crate::support::Dispatch for Runtime {
         match runtime_call{
             RuntimeCall::Balances(call) => {
                 self.balances.dispatch(caller, call)?;
+            },
+            RuntimeCall::ProofOfExistence(call) => {
+                self.proof_of_existence.dispatch(caller, call)?;
             },
         }
         Ok(())
